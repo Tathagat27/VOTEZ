7d0cbd1c4a75fd4aefaa56dd5baa385423db3361
Tathagat27 <tat27302@gmail.com>
2024-08-04 06:10:58 +0530

    fixed build errors

--- client/app/Components/UserHome.js
+++ client/app/Components/UserHome.js
@@ -9,7 +9,7 @@ function UserHome(props) {
     <div className="snap-start snap-always container mx-auto p-4 h-[100vh] flex justify-center items-center">
       <div onClick={() => router.push("/voting")} className="bg-white ring-2 ring-red-200 shadow-xl rounded-xl z-0 px-8 py-12 mb-4 w-[45vw] flex flex-col items-center select-none cursor-pointer transition-transform duration-700 ease-in-out hover:scale-105">
         <h1 className="text-4xl font-bold font-sans bg-gradient-to-tr from-red-600 via-yellow-600 to-orange-900 text-transparent bg-clip-text ">{props.el.electionTitle?.toUpperCase()}</h1>
-        <center className="text-lg font-mono mt-2 text-red-800">~ BY "{props.el.organizationTitle?.toUpperCase()}"</center>
+        <center className="text-lg font-mono mt-2 text-red-800">~ BY &ldquo;{props.el.organizationTitle?.toUpperCase()}&rdquo;</center>
         <table className="mt-6 w-full">
           <tbody>
             <tr>
